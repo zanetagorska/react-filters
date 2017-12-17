9b340aa6c88b3e8553e2db637760d9c1897620ad
Żaneta Górska <zaneta.gorska1@gmail.com>
2017-12-17 10:53:07 +0100

    Add category filters

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,80 @@
 import React, { Component } from 'react';
 
+const products = [
+  {
+    id: 153,
+    name: 'kurtka skórzana',
+    category: 'kurtka'
+  },{
+    id: 242,
+    name: 'ciepła bluza z kapturem',
+    category: 'bluza'
+  }, {
+    id: 343,
+    name: 'rurki jeansowe',
+    category: 'spodnie'
+  },
+];
+const categories = ['kurtka', 'bluza', 'spodnie', 'bielizna'];
+
 export default class App extends Component {
-  render() {
+
+  state = {
+    activeCat: '',
+  }
+
+  updateActiveCat(event) {
+    this.setState({activeCat: event.target.value});
+  }
+
+  renderCategories() {
+    return(
+      categories.map((category, key) => {
+        return(
+          <div key={key}>
+            <input type="radio" id={category} name="contact" value={category} onChange={(e) => this.updateActiveCat(e)}/>
+            <label htmlFor={category}>{category}</label>
+          </div>
+        )
+      })
+    );
+  }
+
+  renderProducts() {
+    const filteredProducts = products.filter(
+      (product) => {
+        return product.category.indexOf(this.state.activeCat) !== -1
+      }
+    )
+
+    console.log(filteredProducts)
+
     return (
-      <div>React simple starter</div>
+      filteredProducts.map((product)=> {
+        return <div key={product.id}>{product.name}</div>
+      })
+    )
+  }
+
+  render() {
+
+
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            Active cat state is: {this.state.activeCat}
+            <form>
+              {this.renderCategories()}
+            </form>
+          </div>
+          <div className="col-sm-6">
+            {this.renderProducts()}
+          </div>
+        </div>
+      </div>
     );
   }
 }
+
